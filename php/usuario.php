915315e1ec06eee94119446d649be706362103b2
Dalia <azudal1102@gmail.com>
2020-01-23 18:49:10 -0600

    Conexion y primer tabla

---
+++ php/usuario.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once("conexion.php");
+class Usuario extends Conexion{
+
+	public function alta($nombre,$tipo,$password){
+		$this->sentencia ="INSERT INTO usuario VALUES (null,'$nombre','$tipo','$password')";
+		$thihs->ejecutarSentencia();
+	}
+
+	public function eliminar($id){
+		$this->sentencia = "DELETE FROM usuario WHERE IDusuario=$id";
+		$this->ejecutarSentencia();
+	}
+
+	public function consulta(){
+		$this->sentencia = "SELECT * FROM usuario";
+		return $this->obetenerSentencia();
+	}
+
+}
+
+ ?>
